aa8a0e1ad9beaf4c30757fa31abf27c68cc48e81
McNultyPT <mcnultypt@gmail.com>
2019-03-14 14:06:59 -0700

    creating monkeyIslandRouter

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const monkeyIslandRouter = require('../monkeyIsland/monkeyIslandRouter.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/monkey-island', monkeyIslandRouter);
 
 server.get('/', async (req, res) => {
     res.status(200).json({ message: 'I am Guybrush Threepwood, mighty pirate!' });
