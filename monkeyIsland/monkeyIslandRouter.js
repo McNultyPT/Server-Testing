f8c3b929475691274205c98e09e716c03546a3d4
McNultyPT <mcnultypt@gmail.com>
2019-03-14 15:07:28 -0700

    writing post request to add a character to db

--- monkeyIsland/monkeyIslandRouter.js
+++ monkeyIsland/monkeyIslandRouter.js
@@ -2,6 +2,13 @@ const router = require('express').Router();
 
 const Chars = require('./monkeyIslandModel.js');
 
-
+router.post('/', async (req, res) => {
+    try {
+        const char = await Chars.add(req.body);
+        res.status(201).json(char);
+    } catch(err) {
+        res.status(500).json({ error: 'Error adding the character.' });
+    }
+});
 
 module.exports = router;
