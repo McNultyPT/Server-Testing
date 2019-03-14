a02397927ea1b87d590c413df41af1141249a5b9
McNultyPT <mcnultypt@gmail.com>
2019-03-14 15:11:16 -0700

    writing delete request to delete a character

--- monkeyIsland/monkeyIslandRouter.js
+++ monkeyIsland/monkeyIslandRouter.js
@@ -11,4 +11,17 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const count = await Chars.remove(req.params.id);
+        if (count > 0) {
+            res.status(204).json({ message: 'Character deleted.' });
+        } else {
+            res.status(404).json({ errorMessage: 'Character not found.' });
+        }
+    } catch(err) {
+        res.status(500).json({ error: 'Error removing the character.' });
+    }
+});
+
 module.exports = router;
